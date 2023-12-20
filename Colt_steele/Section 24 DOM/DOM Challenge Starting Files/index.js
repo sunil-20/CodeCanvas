f7bf827124466ac125ce86f676801a50961e9726
SK <sunil.kafle@hotmail.com>
2023-12-19 22:05:14 -0500

    code changed on DOM

--- Colt_steele/Section 24 DOM/DOM Challenge Starting Files/index.js
+++ Colt_steele/Section 24 DOM/DOM Challenge Starting Files/index.js
@@ -41,3 +41,30 @@ $("h1").css("color", "purple");
 $("h1").css("text-decoration", "underline");
 $("button").html("<em> Don't Click Me</em>")
 $("img").attr("src")
+
+//Traversing Parent/Child/Sibling
+const firstBold = document.querySelector('b')
+
+firstBold.parentElement;
+
+const paragraph = firstBold.parentElement
+
+paragraph.children
+
+paragraph.children[0]
+
+const squareImg = document.querySelector('.square')
+squareImg.parentElement
+// <body> ...</body> << result
+squareImg.children;
+//empty list
+squareImg.nextElementSibling;
+squareImg.previousElementSibling;
+
+const newImg = document.createElement('img')
+//newImg.src = 'https://www.istockphoto.com/photo/happy-dog-owner-with-labrador-puppy-in-her-arms-gm1417321141-464515390'
+newImg.src = 'https://www.secardnepal.org/wp-content/uploads/2017/12/Tent-other-relief-materials-provided-by-SECARD1-1024x768.jpg'
+
+//append to page
+document.body.appendChild(newImg)
+newImg.classList.add('square')
