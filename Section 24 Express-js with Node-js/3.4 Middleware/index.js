b15396cd88381f1d4e823baebb0fc58f355695f4
SK <sunil.kafle@hotmail.com>
2023-12-29 13:02:25 -0500

    code changed

--- Section 24 Express-js with Node-js/3.4 Middleware/index.js
+++ Section 24 Express-js with Node-js/3.4 Middleware/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import bodyParser from "body-parser";
+var colors = require("colors");
 const app = express();
 const port = 3000;
 
@@ -7,6 +8,8 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.post("/submit", (req, res) => {
     console.log(req.body);
 });
+
+console.log('OMG Rainbows!.rainbow');
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}.`);
