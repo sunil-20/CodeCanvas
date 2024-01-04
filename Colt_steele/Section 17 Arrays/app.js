37580d1cdfb4c8cb206c0eabe7aae9310b6d45de
SK <sunil.kafle@hotmail.com>
2024-01-03 20:18:11 -0500

    Sect 17 added

---
+++ Colt_steele/Section 17 Arrays/app.js
@@ -0,0 +1,23 @@
+console.log("I am connected");
+//creating array
+let myArray = [1, 44, "dog", 4.5, "rad"];
+//accessing array
+console.log(myArray[2]);
+console.log(myArray[2][1]);
+console.log(myArray[myArray.length-1]);
+console.log(myArray[4]);
+
+//updating array
+myArray[4] = "red";
+console.log(myArray[4]);
+
+//push, pop, shift, unshift
+
+let movieLine = ['Jack', 'Jill', 'Roma'];
+movieLine.push("Tom");
+movieLine.pop();
+movieLine.shift();
+console.log(movieLine);
+
+movieLine.unshift("Jack is out from here!");
+console.log(movieLine);
