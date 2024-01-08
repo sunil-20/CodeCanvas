993211776a6610e71e2385fb1c74066ae5232c02
SK <sunil.kafle@hotmail.com>
2024-01-07 22:25:17 -0500

    Const and Nested arrays added

--- Colt_steele/Section 17 Arrays/app_184Reference.js
+++ Colt_steele/Section 17 Arrays/app_184Reference.js
@@ -1,19 +1,30 @@
-// let nums = [1,2,3];
-// let numsCopy = nums;
-// console.log(nums);
-// console.log(numsCopy);
-// nums.push(4);
-// console.log(nums);
-// console.log(numsCopy);
-// numsCopy.pop();
-// //nums change as they are linked.
-// console.log(nums);
-// console.log(nums === numsCopy);
+let nums = [1,2,3];
+let numsCopy = nums;
+console.log(nums);
+console.log(numsCopy);
+nums.push(4);
+console.log(nums);
+console.log(numsCopy);
+numsCopy.pop();
+//nums change as they are linked.
+console.log(nums);
+console.log(nums === numsCopy);
 
 
 // 186 Arrays and Const
 
 const PI = 3.1416;
 //can't change the constant.
-PI+= 1
+ PI+= 1
 console.log(PI)
+const nums = [1,2,3];
+nums.push(44);
+//we can alter but we can't entirely assign with same name.
+nums = [1,2,3,44];
+console.log(nums);
+
+//Nested arrays
+
+const GBoard = [['X','O','X'],['O','NULL','X'],['O','O','X']];
+console.log(GBoard);
+console.log(GBoard[1][1])
