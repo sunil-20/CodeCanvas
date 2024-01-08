04e2ee6dbf36540da402039fb444252cf095e9ad
SK <sunil.kafle@hotmail.com>
2024-01-07 22:08:07 -0500

    reference type and equality testing 184

---
+++ Colt_steele/Section 17 Arrays/app_184Reference.js
@@ -0,0 +1,19 @@
+// let nums = [1,2,3];
+// let numsCopy = nums;
+// console.log(nums);
+// console.log(numsCopy);
+// nums.push(4);
+// console.log(nums);
+// console.log(numsCopy);
+// numsCopy.pop();
+// //nums change as they are linked.
+// console.log(nums);
+// console.log(nums === numsCopy);
+
+
+// 186 Arrays and Const
+
+const PI = 3.1416;
+//can't change the constant.
+PI+= 1
+console.log(PI)
