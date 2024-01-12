8391d6b78fcb1980aa4fa32b3e58c2618583af42
SK <sunil.kafle@hotmail.com>
2024-01-12 12:16:46 -0500

    code changed on app.js Sect 19

--- Colt_steele/Section 19 JS Loops/Loops_Code_Vizon/app.js
+++ Colt_steele/Section 19 JS Loops/Loops_Code_Vizon/app.js
@@ -17,3 +17,55 @@ for(let i=2; i<=20; i+=2){
 for (let i=100; i>=0; i-=10){
     console.log(String(i)+" times.")
 }
+
+//looping over arrays
+//length is 5 but index is from 0-4
+const animals = ["tiger","lion","cat","dog","bears"];
+for(i=0; i< animals.length; i++){
+    console.log(i+1, animals[i]);
+ }
+//reverse
+for (let i = animals.length-1; i>=0; i--){
+    console.log(animals[i]);
+}
+
+const people = ["Scooby", "Velma", "Daphne", "Shaggy", "Fred"]; 
+
+for(let i=0; i< people.length; i++){
+    console.log(people[i].toUpperCase());
+}
+
+//nested loop
+for(let i = 1; i <= 10; i++){
+    console.log(`i is ${i}`);
+    for (let j = 1; j<=3; j++){
+        console.log(`      j is: ${j}`)
+    }
+}
+
+const seatingChart = [
+    ['Kristen', 'Erik', 'Namita'],
+    ['Geoffrey', 'Juanita', 'Antonio', 'Kevin'],
+    ['Yuma', 'Sakura', 'Jack', 'Erika']
+]
+// following code will print the array as it is.
+for (let i = 0; i < seatingChart.length; i++){
+    const seatLength = seatingChart[i];
+    console.log(`Row #${[i+1]}`)
+    for (let j = 0; j < seatLength.length; j++){
+    console.log(seatLength[j]);}
+}
+
+// Nested loop to iterate over a 2D array
+let matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+];
+
+for (let i = 0; i < matrix.length; i++) {
+    console.log(`Row #${i+1}`)
+    for (let j = 0; j < matrix[i].length; j++) {
+        console.log(matrix[i][j]);
+    }
+}
