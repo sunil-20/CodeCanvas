f7d99fc05f47463db0adf192339dff928e8d290e
SK <sunil.kafle@hotmail.com>
2024-01-14 00:18:59 -0500

    Project added

---
+++ Colt_steele/Section 19 JS Loops/Loops_CODE_Colt/TodoListProject/Vizon_project/app.js
@@ -0,0 +1,27 @@
+//alert("Connected")
+let input = prompt('what would you like to do?');
+const todos = ['ToDo 1', 'ToDo 2'];
+while(input !== 'quit' && input !== 'q'){
+    if (input === 'list'){
+        console.log('*******************************')
+        for (i = 0; i < todos.length; i++){
+            console.log (`${i}: ${todos[i]}`);
+        }
+        console.log('******************************')
+    } else if(input ==='new'){
+        const newTodo = prompt('Ok, what is the new todo?');
+        todos.push(newTodo);
+        console.log(`${newTodo} added to the list.`)
+    } else if (input === 'delete'){
+        const index = parseInt(prompt('Ok, enter the index to delete:'));
+        if (!Number.isNaN(index)){
+            const deleted = todos.splice(index,1);
+            console.log(`Ok, deleted ${deleted[0]}`);
+        } else{
+            console.log('Unknown index');
+        }
+    }
+    input = prompt("What do you like to do?")
+}
+
+console.log("OK quit the app!")
