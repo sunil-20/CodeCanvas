d565effcf874d69195eeac39460293d50b27ff2b
SK <sunil.kafle@hotmail.com>
2024-01-15 16:02:52 -0500

    code changed

--- Colt_steele/Section 20 Function/app.js
+++ Colt_steele/Section 20 Function/app.js
@@ -26,3 +26,36 @@ function repeat(str, numTimes){
     console.log(result);
 }
 repeat("$", 11)
+//return statement
+function addNum(x,y){
+    let sum = x+y;
+    return sum;
+}
+//create variable that specifies the return value
+const sum = addNum(9,4);
+console.log(sum);
+
+//return false
+function add(x,y){
+    if(typeof x !== 'number'|| typeof y !== 'number'){
+        return false;
+    }
+    return x + y;
+}
+const result = add(add(4,5), 11);
+console.log(result);
+
+//return true if true it returns true, if not it is false already.
+function isShortsWeather(temperature) {
+    return temperature >= 75;
+  }
+
+  //returning last element from an array
+  function lastElement(x){
+    if (x.length !== 0){
+        let last_value = x[x.length-1]
+        return last_value;
+    }
+        return null;
+    }
+x([1,2,3])
