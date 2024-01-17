81f490c770b88ed01c8581ec3b8c4a2598e24084
SK <sunil.kafle@hotmail.com>
2024-01-17 16:02:58 -0500

    code added in Async JS

--- Colt_steele/Section 27 Async JS/app.js
+++ Colt_steele/Section 27 Async JS/app.js
@@ -1,3 +1,4 @@
+//Single thread
 const multiply= (x, y) => x*y;
 const square = x => multiply(x,x);
 const isRightTriangle = (a,b,c) => (
@@ -5,3 +6,17 @@ const isRightTriangle = (a,b,c) => (
 )
 console.log(isRightTriangle(3,4,6))
 
+//ajax implementation // hand off task to browser for help
+console.log("Now sending request to server!")
+setTimeout(()=>{
+    console.log("After the time out I am here.....")
+}, 2000)
+
+let num = 0;
+while (num <50){
+    console.log("I am printed before time out... ");
+    num++;
+    if(num === 49){
+        console.log("SECOND LAST PRINT")
+    }
+}
