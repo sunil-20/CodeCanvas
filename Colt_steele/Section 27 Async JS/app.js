8b399a6a823bd3a20e80c06bda28885df39b5847
SK <sunil.kafle@hotmail.com>
2024-01-17 22:21:10 -0500

    code changed

--- Colt_steele/Section 27 Async JS/app.js
+++ Colt_steele/Section 27 Async JS/app.js
@@ -6,7 +6,7 @@ const isRightTriangle = (a,b,c) => (
 )
 console.log(isRightTriangle(3,4,6))
 
-//ajax implementation // hand off task to browser for help
+// //ajax implementation // hand off task to browser for help
 console.log("Now sending request to server!")
 setTimeout(()=>{
     console.log("After the time out I am here.....")
@@ -20,3 +20,52 @@ while (num <50){
         console.log("SECOND LAST PRINT")
     }
 }
+
+// timeout for background colors
+setTimeout(()=>{
+    document.body.style.backgroundColor = 'red';
+    setTimeout(()=>{
+        document.body.style.backgroundColor = 'orange';
+        setTimeout(() => {
+            document.body.style.backgroundColor = 'yellow';
+            setTimeout(() => {
+                document.body.style.backgroundColor = 'green';
+                setTimeout(() => {
+                    document.body.style.backgroundColor = 'blue';
+                },1000)
+            },1000)
+        },1000)
+    },1000)
+}, 1000)
+
+const delayedColorChange = (color, delay)=>{
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            document.body.style.backgroundColor = color;
+            resolve();
+        }, delay)
+    })
+}
+
+delayedColorChange('red',1000)
+.then(() => delayedColorChange('orange',1000))
+.then(() => delayedColorChange('yellow',1000))
+.then(() => delayedColorChange('green',1000))
+.then(() => delayedColorChange('blue',1000))
+.then(() => delayedColorChange('indigo',1000))
+.then(() => delayedColorChange('violet',1000))
+
+// using await keyword
+
+async function rainbow() {
+    await delayedColorChange('red', 1000)
+    await delayedColorChange('orange', 1000)
+    await delayedColorChange('yellow', 1000)
+    await delayedColorChange('green', 1000)
+    await delayedColorChange('blue', 1000)
+    await delayedColorChange('violet', 1000)
+    return"AlL DONE!"
+
+
+}
+rainbow().then(() => console.log("END OF RAINBOW!"))
