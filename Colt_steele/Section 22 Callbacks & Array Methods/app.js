09496099244fbd80159b8459688ee7c54eba2063
SK <sunil.kafle@hotmail.com>
2024-03-11 12:15:43 -0400

    map method added

--- Colt_steele/Section 22 Callbacks & Array Methods/app.js
+++ Colt_steele/Section 22 Callbacks & Array Methods/app.js
@@ -42,3 +42,59 @@ movies.forEach(function(movie){
 const f = function(movie){
 
 }
+
+// Map methods simple example to multiply with 2
+const numbers = [1, 2, 3, 4, 5];
+const doubledNumbers = numbers.map((num) => num * 2);
+
+console.log(doubledNumbers); // Output: [2, 4, 6, 8, 10]
+
+//capitalize with arrow fx
+const fruits = ['apple', 'banana', 'orange'];
+const capitalizedFruits = fruits.map((fruit) => fruit.toUpperCase());
+console.log(capitalizedFruits); // Output: ['APPLE', 'BANANA', 'ORANGE']
+
+//without arrow function
+const capitalizedFruits2 = fruits.map(function(fruit) {
+  return fruit.toUpperCase();
+});
+
+console.log(capitalizedFruits2); // Output: ['APPLE', 'BANANA', 'ORANGE']
+
+
+//creating new array of movie titles.
+
+const titles = movies.map(function(movie){
+    return movie.title;
+})
+console.log(titles)
+
+// converting to uppercase
+const titles1 = movies.map(function(movie){
+    return movie.title.toUpperCase();
+})
+console.log(titles1)
+
+// using arrow function
+const getTitleUpperCase = (movie) => {
+    return movie.title.toUpperCase();
+};
+
+const titles_ar = movies.map(getTitleUpperCase);
+
+console.log(titles_ar);
+
+//returning scores only from the movies db
+const scores = movies.map(function(movie){
+    return movie.score;
+})
+console.log(scores)
+
+//another example to select only first names.
+const fullNames = [{first: 'Albus', last: 'Dumbledore'}, {first: 'Harry', last: 'Potter'}, {first: 'Hermione', last: 'Granger'}, {first: 'Ron', last: 'Weasley'}, {first: 'Rubeus', last: 'Hagrid'}, {first: 'Minerva', last: 'McGonagall'}, {first: 'Severus', last: 'Snape'}];
+
+const firstNames = fullNames.map(function(fullName){
+    return fullName.first;
+})
+
+console.log(firstNames);
