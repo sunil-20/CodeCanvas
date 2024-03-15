0c5a9d43f875469dc772540c98c64e555043b2b5
SK <sunil.kafle@hotmail.com>
2024-03-15 13:41:37 -0400

    section 23 final commit

--- Colt_steele/Section 23 New JS features/section23.js
+++ Colt_steele/Section 23 New JS features/section23.js
@@ -57,3 +57,109 @@ console.log({...myArray}) //result: { '0': 2, '1': 4, '2': 5, '3': 6, '4': 7 }
 console.log({...myString}) //result: { '0': 'H', '1': 'e', '2': 'l', '3': 'l', '4': 'o', '5': '!' }
 console.log(...myString) //result: H e l l o !
 
+//Rest Params
+function sum(...nums){
+    return nums
+}
+console.log(sum(22,33,33,22,11,44,55,4,5,6,3))
+
+function sum(...nums){
+    return nums.reduce((total, el) => total + el)
+}
+console.log(sum( 1,2,3)) // result: 6
+
+//use case example
+
+function raceResult(gold, silver, ...everyoneElse){
+    console.log(`The Gold medal goes to: ${gold}`)
+    console.log(`The Silver medal goes to: ${silver}`)
+    console.log(`Thanks to everyone else: ${everyoneElse}`) // collecting everyoneElse
+
+
+}
+raceResult('Victor', 'Guru', 'Dina', 'Roshan', 'Jack', 'Tina','Rama')
+//result
+/*
+The Gold medal goes to: Victor
+The Silver medal goes to: Guru
+Thanks to everyone else: Dina,Roshan,Jack,Tina,Rama
+*/
+
+//destructuring from an array
+const scores = [
+    854900, 814953,
+    695836, 290577,
+    646255, 497111,
+    330167
+  ]
+  const [gold, silver, bronz, ...everyoneElse] = scores // making three separate variables, copying 
+  console.log(gold)
+//destructuring from an object(most common)
+
+const user = {
+    email: "example@example.com",
+    password: "password123",
+    firstName: "John",
+    lastName: "Doe",
+    born: 1930,
+    died: 2015,
+    bio: "Tech enthusiast",
+    city: "New York City",
+    state: "New York"
+};
+
+const{email, firstName, lastName} = user; // single them out on their own variable.
+
+console.log(firstName)
+
+//rename the extracted variables
+
+const{born: birthYear, died: deathYear} = user;
+console.log(birthYear) // result: 1930
+
+//assigning the default value
+const{born: birthYear, died: deathYear = 'N/A'} = user;
+
+// destructuring param
+function fullName({firstName, lastName}){ // can provide default value as: lastName = "N/A"
+    return `${firstName} ${lastName}`
+}
+
+console.log(fullName(user))
+
+// using movie example
+const movies = [
+    {
+        title: 'Amadeus',
+        score: 99,
+        year: 2022
+    },
+    {
+        title: 'Stand By Me',
+        score: 85,
+        year: 1988
+    },
+    {
+        title: 'Parasite',
+        score: 95,
+        year: 1967
+    },
+    {
+        title: 'Alien',
+        score: 70,
+        year: 2001
+    }
+]
+
+const mapMovie = movies.map(({title, score, year})=>{
+    return `${title} for the year: (${year}) is rated: ${score}`
+})
+console.log(mapMovie)
+//result
+/*
+[
+  'Amadeus for the year:(2022) is rated: 99',
+  'Stand By Me for the year:(1988) is rated: 85',
+  'Parasite for the year:(1967) is rated: 95',
+  'Alien for the year:(2001) is rated: 70'
+] */
